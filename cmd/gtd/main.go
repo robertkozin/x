c48695dd3db95472c3357b68c6c88c8a42212400
Robert Kozin <robert@robertkozin.com>
2024-06-30 14:44:19 -0400

    gtd: wait for signal to close

--- cmd/gtd/main.go
+++ cmd/gtd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
 }
