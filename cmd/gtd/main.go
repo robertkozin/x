f2b88d1df4d69c0dbecd86b3cc500fd0ebc68d9b
Robert Kozin <robert@robertkozin.com>
2024-06-30 15:10:36 -0400

    basic gtd

--- cmd/gtd/main.go
+++ cmd/gtd/main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
-	"os/signal"
-	"syscall"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
+var started = time.Now()
+
+var index = `
+Hello, World!
+`
+
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("Hello, World!")
 
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
+	Must1(os.MkdirAll("./voice-notes", os.ModePerm))
 
-	<-ctx.Done()
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeContent(w, r, "index.html", started, strings.NewReader(index))
+	})
+
+	http.HandleFunc("POST /voice-note", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("POST /voice-note size=%d\n", r.ContentLength)
+		fname := fmt.Sprintf("%d.mp4", time.Now().Unix())
+		f := Must2(os.Create(filepath.Join("./voice-notes", fname)))
+		defer f.Close()
+
+		Must2(io.Copy(f, r.Body))
+
+		log.Printf("Saved %s\n", fname)
+	})
+
+	log.Println("Listening on port 8080")
+	if err := http.ListenAndServe(":8080", nil); !errors.Is(err, http.ErrServerClosed) {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func Must1(err error) {
+	if err != nil {
+		panic("Must1:" + err.Error())
+	}
+	return
+}
+
+func Must2[T any](v T, err error) T {
+	if err != nil {
+		panic("Must2:" + err.Error())
+	}
+	return v
 }
